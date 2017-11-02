9ebed1e1c5ceb531f0b20aa79c6d0a64afcb59ca
Jan Wilmans <janwilmans@gmail.com>
2017-11-02 20:49:48 +0100

    added formatting files

--- Alarm/vs2017project/main.cpp
+++ Alarm/vs2017project/main.cpp
@@ -5,51 +5,51 @@
 
 namespace Foo
 {
-	void main()
-	{
-		dzn::locator loc;
-		dzn::runtime rt;
-		loc.set(rt);
-		AlarmSystem as(loc);
+  void main()
+  {
+    dzn::locator loc;
+    dzn::runtime rt;
+    loc.set(rt);
+    AlarmSystem as(loc);
 
-		as.console.out.detected = [] {
-			std::cout << "Detected!\n";
-		};
-		as.console.out.deactivated = [] {
-			std::cout << "deactivated!\n";
-		};
+    as.console.out.detected = [] {
+      std::cout << "Detected!\n";
+    };
+    as.console.out.deactivated = [] {
+      std::cout << "deactivated!\n";
+    };
 
-		as.siren.in.turnon = [] {
-			std::cout << "siren on!\n";
-		};
-		as.siren.in.turnoff = [] {
-			std::cout << "siren off!\n";
-		};
+    as.siren.in.turnon = [] {
+      std::cout << "siren on!\n";
+    };
+    as.siren.in.turnoff = [] {
+      std::cout << "siren off!\n";
+    };
 
-		as.sensor.in.enable = [] {
-			std::cout << "sensor enabled\n";
-		};
+    as.sensor.in.enable = [] {
+      std::cout << "sensor enabled\n";
+    };
 
-		as.sensor.in.disable = [] {
-			std::cout << "sensor disabled\n";
-		};
+    as.sensor.in.disable = [] {
+      std::cout << "sensor disabled\n";
+    };
 
-		as.check_bindings();
+    as.check_bindings();
 
-		as.alarm.console.in.arm();
-		as.alarm.sensor.out.triggered();
-	}
-}
+    as.alarm.console.in.arm();
+    as.alarm.sensor.out.triggered();
+  }
+} // namespace Foo
 
 int main(int argc, char* argv[])
 {
-	try
-	{
-		Foo::main();
-	}
-	catch (const std::exception& e)
-	{
-		std::cout << e.what();
-	}
-	return 0;
+  try
+  {
+    Foo::main();
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what();
+  }
+  return 0;
 }
