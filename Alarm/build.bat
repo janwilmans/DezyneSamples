ede7c852ae991ad05c44ff1706f039f1e55813b0
Jan Wilmans <janwilmans@gmail.com>
2018-05-26 01:41:17 +0200

    fixed build output

--- Alarm/build.bat
+++ Alarm/build.bat
@@ -15,12 +15,12 @@ set language=c++-msvc11
 set version=2.7.2
 
 call DezyneGenerateCode.bat
-msbuild vs2013project\Alarm.sln %REBUILD_X86_RELEASE$ %MSBUILDLOGGER% %OUTPUT%
+msbuild vs2013project\Alarm.sln %REBUILD_X86_RELEASE% %MSBUILDLOGGER% %OUTPUT%
 
 set version=development
 call DezyneGenerateCode.bat
-msbuild vs2013project\Alarm.sln %REBUILD_X86_RELEASE$ %MSBUILDLOGGER% %OUTPUT_APPEND%
+msbuild vs2013project\Alarm.sln %REBUILD_X86_RELEASE% %MSBUILDLOGGER% %OUTPUT_APPEND%
 
 set language=c++
 set version=development
-msbuild vs2017project\Alarm2017.sln %REBUILD_X86_RELEASE$ %MSBUILDLOGGER% %OUTPUT_APPEND%
+msbuild vs2017project\Alarm2017.sln %REBUILD_X86_RELEASE% %MSBUILDLOGGER% %OUTPUT_APPEND%
