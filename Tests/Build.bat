ea500b94a56ed62296ef817fe6dcff2be9044e72
Jan Wilmans <janwilmans@gmail.com>
2018-05-26 01:12:36 +0200

    build and test

--- Tests/Build.bat
+++ Tests/Build.bat
@@ -8,8 +8,8 @@ if [%APPVEYOR%]==[True] (
 
 set REBUILD_X86_RELEASE=/p:Platform="x86" /p:Configuration=Release /t:Rebuild
 
-msbuild Tests\Tests.sln %REBUILD_X86_RELEASE% /t:Rebuild %MSBUILDLOGGER%
+msbuild Tests.sln %REBUILD_X86_RELEASE% /t:Rebuild %MSBUILDLOGGER%
 
 if NOT [%APPVEYOR%]==[True] (
-    Tests\Release\Tests.exe -s -i -w NoTests
+    Release\Tests.exe -s -i -w NoTests
 )
