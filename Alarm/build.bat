4b23dc6b4adaf441f82cd18ef741b43fe326e1a5
Jan Wilmans <janwilmans@gmail.com>
2018-05-26 01:07:24 +0200

    add UploadTestResults.ps1

--- Alarm/build.bat
+++ Alarm/build.bat
@@ -2,18 +2,24 @@
 pushd
 cd %~dp0
 
+if [%APPVEYOR%]==[True] (
+  SETLOCAL MSBUILDLOGGER /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+)
+
+set REBUILD_X86_RELEASE=/p:Platform="x86" /p:Configuration=Release /t:Rebuild
+
 set generated_file_dirname=generated
 set runtime_dir_name=runtime
 set language=c++-msvc11
 set version=2.7.2
 
 call DezyneGenerateCode.bat
-msbuild vs2013project\Alarm.sln /p:Configuration=Release /t:Rebuild /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+msbuild vs2013project\Alarm.sln %REBUILD_X86_RELEASE$ %MSBUILDLOGGER%
 
 set version=development
 call DezyneGenerateCode.bat
-msbuild vs2013project\Alarm.sln /p:Configuration=Release /t:Rebuild /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+msbuild vs2013project\Alarm.sln %REBUILD_X86_RELEASE$ %MSBUILDLOGGER%
 
 set language=c++
 set version=development
-msbuild vs2017project\Alarm2017.sln /p:Configuration=Release /t:Rebuild /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+msbuild vs2017project\Alarm2017.sln %REBUILD_X86_RELEASE$ %MSBUILDLOGGER%
--- BuildTests.bat
+++ BuildTests.bat
@@ -6,7 +6,9 @@ if [%APPVEYOR%]==[True] (
   SETLOCAL MSBUILDLOGGER /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 )
 
-msbuild Tests\Tests.sln /p:Configuration=Release /t:Rebuild %MSBUILDLOGGER%
+set REBUILD_X86_RELEASE=/p:Platform="x86" /p:Configuration=Release /t:Rebuild
+
+msbuild Tests\Tests.sln %REBUILD_X86_RELEASE% /t:Rebuild %MSBUILDLOGGER%
 
 if NOT [%APPVEYOR%]==[True] (
     Tests\Release\Tests.exe -s -i -w NoTests
