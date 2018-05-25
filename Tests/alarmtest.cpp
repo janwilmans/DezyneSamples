d3225c17dcfeaf160fc7b8fe928bf07962418e24
Jan Wilmans <janwilmans@gmail.com>
2018-05-26 01:38:12 +0200

    added warning detection

--- Tests/alarmtest.cpp
+++ Tests/alarmtest.cpp
@@ -5,9 +5,30 @@ unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+std::string readfile(const std::string& filename)
+{
+    // reads upto the first \0 character or the whole file if not found
+    std::fstream f(filename, std::fstream::in);
+    std::string s;
+    std::getline(f, s, '\0');
+    return s;
+}
+
+TEST_CASE( "Analyze warnings", "[warning(s)]" ) {
+
+    std::string resultfile = "..\\..\\build.txt";
+    std::fstream f(resultfile, std::fstream::in);
+    
+    int warnings = 0;
+    std::string s;
+    while (!f.eof())
+    {
+        getline(f, s);
+        if (s.find("warning") != std::string::npos)
+        {
+            std::cerr << s;
+            warnings++;
+        }
+    }
+    REQUIRE(warnings == 0);
 }
