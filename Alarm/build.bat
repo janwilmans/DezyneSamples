a7a7d83bfe54904f3d622c2dc393038db0294747
Jan Wilmans <janwilmans@gmail.com>
2018-05-26 21:25:48 +0200

    resume tests

--- Alarm/build.bat
+++ Alarm/build.bat
@@ -1,10 +1,16 @@
 @echo on
 cd %~dp0
 
+:: SETLOCAL will start localisation of environment variables
+setlocal enableDelayedExpansion
+
+set "OUTPUT=>build.txt 2>&1"
+set "OUTPUT_APPEND=>>build.txt 2>&1"
+
 if [%APPVEYOR%]==[True] (
-  SETLOCAL MSBUILDLOGGER /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
-  SETLOCAL OUTPUT >%APPVEYOR_BUILD_FOLDER%\build.txt 2>&1
-  SETLOCAL OUTPUT_APPEND >>%APPVEYOR_BUILD_FOLDER%\build.txt 2>&1
+  set MSBUILDLOGGER /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+  set "OUTPUT=>build.txt 2>&1"
+  set "OUTPUT_APPEND=>>build.txt 2>&1"
 )
 
 set REBUILD_X86_RELEASE=/p:Platform="x86" /p:Configuration=Release /t:Rebuild
@@ -15,7 +21,7 @@ set language=c++-msvc11
 set version=2.7.2
 
 call DezyneGenerateCode.bat
-msbuild vs2013project\Alarm.sln %REBUILD_X86_RELEASE% %MSBUILDLOGGER% %OUTPUT%
+msbuild vs2013project\Alarm.sln %REBUILD_X86_RELEASE% %MSBUILDLOGGER% %OUTPUT% 
 
 set version=development
 call DezyneGenerateCode.bat
@@ -24,3 +30,4 @@ msbuild vs2013project\Alarm.sln %REBUILD_X86_RELEASE% %MSBUILDLOGGER% %OUTPUT_AP
 set language=c++
 set version=development
 msbuild vs2017project\Alarm2017.sln %REBUILD_X86_RELEASE% %MSBUILDLOGGER% %OUTPUT_APPEND%
+
